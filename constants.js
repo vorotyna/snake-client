9d9a63c55d2fd71b415151cdb2c56bb066ec693e
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-26 22:00:02 +0000

    File with constants for strings and other data that are static in my code

---
+++ constants.js
@@ -0,0 +1,19 @@
+// File with constants for strings and other data that are "static" in my code
+const host = '165.227.47.243';
+const port = 50541;
+
+const moveUp = 'w';
+const moveDown = 's';
+const moveLeft = 'a';
+const moveRight = 'd';
+const message = "p";
+
+module.exports = {
+  host,
+  port,
+  moveUp,
+  moveDown,
+  moveLeft,
+  moveRight,
+  message
+};
