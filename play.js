2b5d0a510aa5861e941fdcf0187be69b3f638238
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-26 16:13:18 +0000

    Return 'you ded cuz you idled' message

--- play.js
+++ play.js
@@ -10,6 +10,12 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.setTimeout(5000);
+  conn.on('timeout', () => {
+    console.log('you ded cuz you idled');
+    conn.end();
+  });
+
   return conn;
 };
 
