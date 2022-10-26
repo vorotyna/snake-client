3cd7a2ee16804784d6ad1b42f43da24bf92131c9
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-26 22:00:36 +0000

    Importing host and port variables

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { host, port } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
+    host, // IP address
+    port // PORT number
   });
 
   // interpret incoming data as text
