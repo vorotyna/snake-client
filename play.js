70caa134b8b97c95abe9bfd042ecf7ea010cde44
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-26 19:18:44 +0000

    Removing the connect function and importing it in using ES6 Shorthand syntax

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const net = require("net");
-
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.setTimeout(5000);
-  conn.on('timeout', () => {
-    console.log('you ded cuz you idled');
-    conn.end();
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
